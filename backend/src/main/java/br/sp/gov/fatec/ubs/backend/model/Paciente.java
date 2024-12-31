4348fea8d1c4b145db2c227aeeb7d1c8f1560307
klebinho28 <klebernunesdeassis0@gmail.com>
2024-12-31 00:07:30 -0300

    Mudanças em Andamento

--- backend/src/main/java/br/sp/gov/fatec/ubs/backend/model/Paciente.java
+++ backend/src/main/java/br/sp/gov/fatec/ubs/backend/model/Paciente.java
@@ -6,38 +6,217 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity(name="paciente")
+@Entity(name = "paciente")
 public class Paciente {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    private String nome;
-    
-    @Column(unique=true)
-    private String cpf;
 
-    public long getId() {
-        return id;
+    private Long codigo;
+    private String nomeCompleto;
+    private String nomeSocial;
+    private String nomeMae;
+    private String nomePai;
+    private String dataNascimento;
+    private String sexo;
+    private String Nacionalidade;
+    private String MunicipioNascimento;
+    private String Raca;
+    private String Situacaofamiliar;
+    private String FrequentaEscola;
+    private String EstabelecimentoVeiculo;
+    private String EstabelecimentoCadastro;
+    private String deficiencia;
+    private String visual;
+    private String auditiva;
+    private String motora;
+    private String intelectual;
+    private String TelefoneCelular;
+    private String TelefoneComercial;
+    private String Contato;
+    private String Email;
+
+    public Long getCodigo() {
+        return codigo;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
     }
 
-    public String getNome() {
-        return nome;
+    public String getNomeCompleto() {
+        return nomeCompleto;
     }
 
-    public String getCpf() {
-        return cpf;
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
     }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
+    public String getNomeSocial() {
+        return nomeSocial;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setNomeSocial(String nomeSocial) {
+        this.nomeSocial = nomeSocial;
     }
-    
+
+    public String getNomeMae() {
+        return nomeMae;
+    }
+
+    public void setNomeMae(String nomeMae) {
+        this.nomeMae = nomeMae;
+    }
+
+    public String getNomePai() {
+        return nomePai;
+    }
+
+    public void setNomePai(String nomePai) {
+        this.nomePai = nomePai;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNacionalidade() {
+        return Nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        Nacionalidade = nacionalidade;
+    }
+
+    public String getMunicipioNascimento() {
+        return MunicipioNascimento;
+    }
+
+    public void setMunicipioNascimento(String municipioNascimento) {
+        MunicipioNascimento = municipioNascimento;
+    }
+
+    public String getRaca() {
+        return Raca;
+    }
+
+    public void setRaca(String raca) {
+        Raca = raca;
+    }
+
+    public String getSituacaofamiliar() {
+        return Situacaofamiliar;
+    }
+
+    public void setSituacaofamiliar(String situacaofamiliar) {
+        Situacaofamiliar = situacaofamiliar;
+    }
+
+    public String getFrequentaEscola() {
+        return FrequentaEscola;
+    }
+
+    public void setFrequentaEscola(String frequentaEscola) {
+        FrequentaEscola = frequentaEscola;
+    }
+
+    public String getEstabelecimentoVeiculo() {
+        return EstabelecimentoVeiculo;
+    }
+
+    public void setEstabelecimentoVeiculo(String estabelecimentoVeiculo) {
+        EstabelecimentoVeiculo = estabelecimentoVeiculo;
+    }
+
+    public String getEstabelecimentoCadastro() {
+        return EstabelecimentoCadastro;
+    }
+
+    public void setEstabelecimentoCadastro(String estabelecimentoCadastro) {
+        EstabelecimentoCadastro = estabelecimentoCadastro;
+    }
+
+    public String getDeficiencia() {
+        return deficiencia;
+    }
+
+    public void setDeficiencia(String deficiencia) {
+        this.deficiencia = deficiencia;
+    }
+
+    public String getVisual() {
+        return visual;
+    }
+
+    public void setVisual(String visual) {
+        this.visual = visual;
+    }
+
+    public String getAuditiva() {
+        return auditiva;
+    }
+
+    public void setAuditiva(String auditiva) {
+        this.auditiva = auditiva;
+    }
+
+    public String getMotora() {
+        return motora;
+    }
+
+    public void setMotora(String motora) {
+        this.motora = motora;
+    }
+
+    public String getIntelectual() {
+        return intelectual;
+    }
+
+    public void setIntelectual(String intelectual) {
+        this.intelectual = intelectual;
+    }
+
+    public String getTelefoneCelular() {
+        return TelefoneCelular;
+    }
+
+    public void setTelefoneCelular(String telefoneCelular) {
+        TelefoneCelular = telefoneCelular;
+    }
+
+    public String getTelefoneComercial() {
+        return TelefoneComercial;
+    }
+
+    public void setTelefoneComercial(String telefoneComercial) {
+        TelefoneComercial = telefoneComercial;
+    }
+
+    public String getContato() {
+        return Contato;
+    }
+
+    public void setContato(String contato) {
+        Contato = contato;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
 }
--- frontend/src/app/model/paciente.ts
+++ frontend/src/app/model/paciente.ts
@@ -1,5 +1,42 @@
 export class Paciente {
-    public id: number = 0;
-    public nome: string = "";
-    public cpf: string = "";
+
+  public codigo?: number;
+  public nome: string ='';
+  public nomeSocial?: string ='';
+  public nomeMae?: string ='';
+  public nomePai?:string = '';
+
+
+  //Dados de Nascimento e Sexo
+  public dataNascimento = new Date();
+  public sexo:'Masculino'| 'Feminino'| 'Outro' = 'Feminino';
+
+  //Loaclidade
+  public Raca!: 'Branca' | 'Preta' | 'Parda' | 'Amarela' | 'Indigina';
+  public Nacionalidade:string ='';
+  public MunicipioNascimento:string='';
+  public FrequentaEscola?: 'Sim' | 'Não';
+
+
+  public deficiencia?: 'Sim' | 'Não';
+  public visual?: 'Sim' | 'Não';
+  public auditiva?: 'Sim'| 'Não';
+  public motora?: 'Sim' | 'Não';
+  public intelectual?: 'Sim' | 'Não';
+
+  //Dados de Contato
+
+ public contatoCelular:string='';
+ public contatoResidencial:string='';
+ public contatoComercial:string='';
+ public contatoEmail:string='';
+
+ constructor(init?:Partial<Paciente>){
+  Object.assign(this, init);
+ }
+
+
+
+
+
 }
